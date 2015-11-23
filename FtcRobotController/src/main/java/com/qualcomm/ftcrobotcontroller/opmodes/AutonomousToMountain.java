2033b6b932a56805339a60fa48d3422a7ce61f48
Joshua M. Atler <joshatler@gmail.com>
2015-11-23 15:56:03 -0800

    AutonomousToMountain (still not working).

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousToMountain.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousToMountain.java
@@ -1,59 +1,93 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
 
-/**
- * Created by Joshua on 11/4/2015.
- */
-public class AutonomousToMountain extends OpMode {
+public class AutonomousToMountain extends LinearOpMode{
 
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    private DcMotor left;
+    private DcMotor right;
 
     @Override
-    public void init() {
-        leftMotor = hardwareMap.dcMotor.get("left");
-        rightMotor = hardwareMap.dcMotor.get("right");
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
-        rightMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
-        leftMotor.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    public void runOpMode() throws InterruptedException {
+        left = hardwareMap.dcMotor.get("left");
+        right = hardwareMap.dcMotor.get("right");
+        left.setDirection(DcMotor.Direction.REVERSE);
+
+        System.out.println("1");
+        waitForStart();
+
+        System.out.println("2");
+        resetEncoders();
+
+        System.out.println(3);
+        driveDistance(-3000, 0.5);
+        System.out.println(4);
+        turnDistance(1000, 0.5);
+        System.out.println(5);
+        driveDistance(-6000, 0.5);
+        System.out.println(6);
+        turnDistance(-2000, 0.5);
+        System.out.println(7);
+        driveDistance(5000, 0.5);
     }
 
-    @Override
-    public void start() {
-        driveDistance(1000, 0.3F);
-//        turnDistance(-5000, 0.75F);
-//        driveDistance(-3000, -0.75F);
-//        turnDistance(1000, 0.75F);
-//        driveDistance(5000, 0.75F);
+    private void driveDistance(int distance, double power) throws InterruptedException{
+        setPos(left, distance);
+        setPos(right, distance);
+        left.setPower(power);
+        right.setPower(power);
+        while(!posReached()) {
+            telemetry.addData("Left", left.getCurrentPosition());
+            telemetry.addData("Right", right.getCurrentPosition());
+            telemetry.addData("LT", left.getTargetPosition());
+            telemetry.addData("RT", right.getTargetPosition());
+            waitOneFullHardwareCycle();
+        }
+        resetEncoders();
     }
 
-    @Override
-    public void loop() {
-        telemetry.addData("Left", leftMotor.getCurrentPosition());
-        telemetry.addData("Right", rightMotor.getCurrentPosition());
-        telemetry.addData("LeftTarget", leftMotor.getTargetPosition());
+    private void turnDistance(int distance, double power) throws InterruptedException{
+        setPos(left, distance);
+        setPos(right, -distance);
+        left.setPower(power);
+        right.setPower(power);
+        while(!posReached()) {
+            telemetry.addData("Left", left.getCurrentPosition());
+            telemetry.addData("Right", right.getCurrentPosition());
+            telemetry.addData("LT", left.getTargetPosition());
+            telemetry.addData("RT", right.getTargetPosition());
+            waitOneFullHardwareCycle();
+        }
+        resetEncoders();
     }
 
-    void driveDistance(int encoderDistance, float power) {
-        rightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        leftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        rightMotor.setTargetPosition(encoderDistance);
-        leftMotor.setTargetPosition(encoderDistance);
-        rightMotor.setPower(power);
-        leftMotor.setPower(power);
+    private void resetEncoders() throws InterruptedException{
+        left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        while(!haveEncodersReset())
+            waitOneFullHardwareCycle();
     }
 
-    void turnDistance(int encoderDistance, float power) {
-        rightMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
-        leftMotor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+    private void setPos(DcMotor motor, int pos){
+        motor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(pos);
+    }
 
-        leftMotor.setTargetPosition(encoderDistance);
-        rightMotor.setTargetPosition(-encoderDistance);
-        leftMotor.setPower(power);
-        rightMotor.setPower(power);
+    private boolean posReached(){
+        return hasEncoderReachedPosition(left) && hasEncoderReachedPosition(right);
+    }
+
+
+    private boolean hasEncoderReachedPosition(DcMotor motor){
+        return (Math.abs(motor.getCurrentPosition() - motor.getTargetPosition()) < 3);
+    }
+    private boolean haveEncodersReset(){
+        return hasEncoderReset(left) && hasEncoderReset(right);
+    }
+    private boolean hasEncoderReset(DcMotor motor){
+        return motor.getCurrentPosition() == 0;
     }
 }
