4fba2c0c67c78b07af4f64802acfcbd408cd2cb8
Joshua M. Atler <joshatler@gmail.com>
2016-01-14 17:03:52 -0800

    Started working on implementing gyro code into the state machine.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SMClimberDump.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/SMClimberDump.java
@@ -1,11 +1,15 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
+import com.qualcomm.ftcrobotcontroller.common.GyroWorkerThread;
 import com.qualcomm.ftcrobotcontroller.common.RedBlueOpMode;
 import com.qualcomm.ftcrobotcontroller.common.StateMachine;
 import com.qualcomm.ftcrobotcontroller.common.Values;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
+
+import java.nio.channels.DatagramChannel;
 
 /**
  * Created by Joshua on 1/13/2016.
@@ -19,7 +23,17 @@ public class SMClimberDump extends RedBlueOpMode {
     private DcMotor hangArm;
     private DcMotor plow;
 
+    GyroSensor gyroSensor;
+    GyroWorkerThread gyro;
+    double leftPower;
+    double rightPower;
+
     Servo climberServo;
+
+
+    double power;
+    double target;
+    int distance;
 
 
     @Override
@@ -29,6 +43,9 @@ public class SMClimberDump extends RedBlueOpMode {
         right = hardwareMap.dcMotor.get("right");
         plow = hardwareMap.dcMotor.get("plow");
         hangArm = hardwareMap.dcMotor.get("hangArm");
+        gyroSensor = this.hardwareMap.gyroSensor.get("gyro");
+        gyro = new GyroWorkerThread(this, gyroSensor);
+        gyro.start();
 
         climberServo = hardwareMap.servo.get("climber");
 
@@ -47,6 +64,47 @@ public class SMClimberDump extends RedBlueOpMode {
 
     }
 
+    private void updateGyro(){
+            telemetry.addData("Left", left.getCurrentPosition());
+            telemetry.addData("Right", right.getCurrentPosition());
+            telemetry.addData("LeftPower", left.getPower());
+            telemetry.addData("RightPower", right.getPower());
+
+            if(distance > 0) {
+                leftPower = power - (gyro.heading() - target) / 200;
+                rightPower = power + (gyro.heading() - target) / 200;
+            } else {
+                leftPower = power + (gyro.heading() - target) / 200;
+                rightPower = power - (gyro.heading() - target) / 200;
+            }
+
+            if(leftPower > 0.5)
+                leftPower = 0.5;
+            else if(leftPower < 0.1)
+                leftPower = 0.1;
+            left.setPower(leftPower);
+
+            if(rightPower > 0.5)
+                rightPower = 0.5;
+            else if(rightPower < 0.1)
+                rightPower = 0.1;
+            right.setPower(rightPower);
+
+
+
+    }
+
+    private void driveGyroDistance(int distance, double power, int target) {
+        this.distance = distance;
+        this.power = power;
+        this.target = target;
+    }
+
+    private void setPos(DcMotor motor, int pos){
+        motor.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        motor.setTargetPosition(pos);
+    }
+
     private void resetEncoders() {
         left.setMode(DcMotorController.RunMode.RESET_ENCODERS);
         right.setMode(DcMotorController.RunMode.RESET_ENCODERS);
@@ -62,6 +120,19 @@ public class SMClimberDump extends RedBlueOpMode {
     private void setRunToPosition() {
         left.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
         right.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+
+        plow.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+        hangArm.setMode(DcMotorController.RunMode.RUN_TO_POSITION);
+    }
+
+    private void resetPlowAndHangArm() {
+        hangArm.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+        plow.setMode(DcMotorController.RunMode.RESET_ENCODERS);
+    }
+
+    public void stopMotors() {
+        left.setPower(0);
+        right.setPower(0);
     }
 
     enum States implements StateMachine.State {
@@ -76,6 +147,7 @@ public class SMClimberDump extends RedBlueOpMode {
             @Override
             public void runState() {
                 parent.resetEncoders();
+                parent.resetPlowAndHangArm();
                 parent.climberServo.setPosition(Values.CLIMBER_CLOSE);
             }
 
@@ -89,6 +161,63 @@ public class SMClimberDump extends RedBlueOpMode {
 
             }
         },
+        PLOW_DOWN {
+            @Override
+            public boolean shouldChangeState() {
+                if (Math.abs(parent.plow.getCurrentPosition() - Values.PLOW_DEPLOY) < 3)
+                    return true;
+                else
+                    return false;
+            }
+
+            @Override
+            public void runState() {
+                parent.plow.setTargetPosition(Values.PLOW_DEPLOY);
+                parent.plow.setPower(0.5);
+            }
+
+            @Override
+            public void end() {
+                parent.setRunToPosition();
+            }
+
+            @Override
+            public void tick() {
+
+            }
+
+            SMClimberDump parent;
+        },
+        DRIVE_BACK_1{
+            SMClimberDump parent;
+
+            @Override
+            public boolean shouldChangeState() {
+                if((Math.abs(parent.left.getCurrentPosition() - parent.distance) < 3) && (Math.abs(parent.right.getCurrentPosition() - parent.distance) < 3))
+                    return true;
+                else
+                    return false;
+
+            }
+
+            @Override
+            public void runState() {
+                parent.driveGyroDistance(-7000, -0.5, 0);
+            }
+
+            @Override
+            public void end() {
+                parent.stopMotors();
+            }
+
+            @Override
+            public void tick() {
+                parent.updateGyro();
+            }
+        }
+
+
+        /*
         DRIVE_FORWARD {
             SMClimberDump parent;
 
@@ -170,5 +299,7 @@ public class SMClimberDump extends RedBlueOpMode {
 
             }
         }
+
+        */
     }
 }
