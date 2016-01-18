f89917d300f16f3fc638beb9a9eb604777eed1b9
Austin Whyte <mr.austinwhyte@gmail.com>
2016-01-18 13:31:10 -0800

    Swapped deploy and retract values

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/common/Values.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/common/Values.java
@@ -12,10 +12,17 @@ public class Values {
     //Nathan B adjusted CLIMBER_OPEN to 1 for better climber release. Original Value: 0.29803923
     public static final double CLIMBER_OPEN = 1;
     public static final double CLIMBER_CLOSE = 0;
+//
+//    public static final double TRIGGER_LEFT_DEPLOY = 0.105882354;
+//    public static final double TRIGGER_LEFT_RETRACT = 1;
+//
+//    public static final double TRIGGER_RIGHT_DEPLOY = 1;
+//    public static final double TRIGGER_RIGHT_RETRACT = 0.23137255;
 
-    public static final double TRIGGER_LEFT_DEPLOY = 0.105882354;
-    public static final double TRIGGER_LEFT_RETRACT = 1;
 
-    public static final double TRIGGER_RIGHT_DEPLOY = 1;
-    public static final double TRIGGER_RIGHT_RETRACT = 0.23137255;
+    public static final double TRIGGER_LEFT_DEPLOY = 1;
+    public static final double TRIGGER_LEFT_RETRACT = 0.105882354;
+
+    public static final double TRIGGER_RIGHT_DEPLOY = 0.23137255;
+    public static final double TRIGGER_RIGHT_RETRACT = 1;
 }
